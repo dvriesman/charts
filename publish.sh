7e7541245ef520519f91c5efc005bee8f592e93e
Denny Richard San Vriesman <dvriesman@gmail.com>
2019-11-25 22:20:16 -0300

    my file

--- publish.sh
+++ publish.sh
@@ -5,7 +5,7 @@ HELM_HOME=/var/jenkins_home/linux-amd64
 rm *.tgz
 $HELM_HOME/helm lint test1
 $HELM_HOME/helm package test1
-curl --data-binary "@test1-0.1.0.tgz" http://localhost:9080/api/charts
+curl --data-binary "@test1-0.1.0.tgz" http://localhost:9085/api/charts
 
 exit
 
